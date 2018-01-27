66c1fdf8fb0b00c3e5b30f808f25298cca4e4f55
Raja <raja.muthukalai@yahoo.com>
2018-01-27 19:35:33 +0530

    config server base code

---
+++ src/main/java/com/sannu/configserver/ConfigServerApplication.java
@@ -0,0 +1,28 @@
+package com.sannu.configserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
+
+@RestController
+class DefaultController {
+	
+	@Value("${message:default message}")
+	private String message;
+	
+	@GetMapping("/message")
+	public String message() {
+		return message;
+	}
+}
